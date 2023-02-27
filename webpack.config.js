39f93643f21b3d2b28143792407c2fbe27c2f605
Daria Krushelnitskaya <dasha_fr_russia@mail.ru>
2023-02-27 03:44:33 +0300

    dep

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	devtool: 'inline-source-map',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[hash].js',
+		filename: 'main.js',
 		publicPath: '',
 	},
 	mode: 'development',
